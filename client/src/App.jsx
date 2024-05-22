49d328c78bb5c73375e981e811f4e6e0f893fa8a
Mazen Afifi <mazen.afifi@flairstech.com>
2024-05-22 14:56:31 +0300

    pages and routes created successfully

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import DashBoard from './pages/DashBoard';
+import Projects from './pages/Projects'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/dashBoard' element={<DashBoard/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
