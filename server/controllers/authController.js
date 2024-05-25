b30d50b02902cd387a0a19a0b9bae7893c712e23
Mazen Afifi <mazen.afifi@flairstech.com>
2024-05-25 14:44:05 +0300

    error handlers middelware and function created successfully

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,14 +1,16 @@
 import User from "../models/UserModel.js";
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
 
 
 
 
-export const signUp = async(req,res)=>{
+export const signUp = async(req,res , next)=>{
     // console.log(req.body);
     const {username , email,password} = req.body;
     if(! username || !email || !password || username ===''|| email ==='' || password === ''){
-        return res.status(401).json({message:"All fields are required!"})
+        // return res.status(400).json({message:"All fields are required!"})
+        next(errorHandler('400' , 'All fields are required'));
     }
     let newUser;
     const hashedPassword = bcryptjs.hashSync(password , 10)
@@ -21,7 +23,8 @@ export const signUp = async(req,res)=>{
             await newUser.save();
             return res.status(200).json({message:"User is Created successfully"})
         } catch (error) {
-            console.log(error.message);
+            // console.log(error.message);
+            next(error)
             return res.status(500).json({message:"Something went wrong while creating New User!" , error: error.message})
         }
 }
