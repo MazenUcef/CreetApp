3f5b2f62ff0de8d75e1172c5948926dd10ebf7d7
Mazen Afifi <mazen.afifi@flairstech.com>
2024-06-20 02:10:45 +0300

    preBuild and deploy the project done successfully

--- server/index.js
+++ server/index.js
@@ -7,37 +7,47 @@ import authRoutes from './routes/authRoute.js'
 import postRoutes from './routes/post.routes.js'
 import commentRoutes from './routes/commentRoutes.js'
 import cookieParser from 'cookie-parser';
+import path from 'path'
 
- 
+
 dotenv.config();
 
+const __dirname = path.resolve()
 
-const app = express(); 
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser())
 
 
-mongoose.set('strictQuery' , false);
+mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO)
-.then(()=>console.log('Database Connected Successfully'))
-.catch(err=> console.log('Error happened while connecting to database' , err.message))
+    .then(() => console.log('Database Connected Successfully'))
+    .catch(err => console.log('Error happened while connecting to database', err.message))
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
     console.log('server is running on port 5000');
 })
 
 
-app.use('/api/user' , userRoutes)
-app.use('/api/auth' , authRoutes)
-app.use('/api/post' , postRoutes)
-app.use('/api/comment' , commentRoutes)
-app.use((err,req,res,next)=>{
+
+app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/post', postRoutes)
+app.use('/api/comment', commentRoutes)
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
     res.status(statusCode).json({
-        success:false,
+        success: false,
         statusCode,
         message
     })
- })
+})
