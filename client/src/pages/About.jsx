3f9a39be067c0985cc054746174ed23df26146e9
Mazen Afifi <mazen.afifi@flairstech.com>
2024-06-20 02:01:42 +0300

    projects and about topic done succeessfully

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='text-center max-w-2xl mx-auto p-3'>
+        <div className='flex flex-col items-center justify-center'>
+          <h1 className='text-6xl font-bold text-primary mt-5 mb-10'>About <span className='text-secondary'>_Creet</span> Blog</h1>
+          <div className='flex flex-col items-center justify-center'>
+            <p className='text-sm text-third'>Welcome to Creet Blog, your ultimate destination for insightful articles, inspiring stories, and in-depth analyses across a wide range of topics. At Creet Blog, we believe in the power of words to inform, inspire, and ignite change. Our mission is to create a vibrant community where readers can find meaningful content that resonates with their interests and sparks their curiosity.</p>
+            <h1 className='text-3xl font-bold text-primary mt-5 mb-5'>Our Mission</h1>
+            <p className='text-sm text-third'>
+              Our mission at Creet Blog is to provide a platform that elevates diverse voices and perspectives. We aim to deliver high-quality content that not only informs but also engages and inspires our readers. Whether you're looking for the latest trends in technology, in-depth reviews of the newest books, or personal stories that touch the heart, Creet Blog has something for everyone.
+            </p>
+            <h1 className='text-3xl font-bold text-primary mt-5 mb-5'>What We Offer</h1>
+            <p className='text-sm text-third'>At Creet Blog, we cover a broad spectrum of topics to cater to our diverse readership. Our categories include:</p>
+            <p className='text-sm text-third'>Technology: Stay updated with the latest advancements, trends, and innovations in the tech world. From AI breakthroughs to gadget reviews, we've got you covered.
+              Lifestyle: Explore articles on health, wellness, travel, and more. Discover tips and tricks to enhance your daily life and well-being.
+              Culture: Dive into thought-provoking pieces on art, literature, history, and more. Celebrate the richness of diverse cultures and heritage.
+              Personal Development: Find motivation and guidance on personal growth, career advancement, and achieving your goals. Learn from the experiences and wisdom of others.
+              Current Affairs: Get informed about the latest news and events happening around the world. Understand the context and implications of major developments.
+            </p>
+            <h1 className='text-3xl font-bold text-primary mt-5 mb-5'>Our Writers</h1>
+            <p className='text-sm text-third'>Our team of dedicated writers and contributors brings a wealth of knowledge and experience to Creet Blog. Each writer is passionate about their subject matter and committed to delivering well-researched, insightful, and engaging content. We pride ourselves on fostering a community of talented individuals who share our vision of creating a space for meaningful dialogue and learning.</p>
+            <h1 className='text-sm text-primary mt-5 font-bold'>Thank you for visiting Creet Blog. We hope you find our articles enlightening, our stories inspiring, and our community welcoming. Together, let's explore the world of ideas, one article at a time.</h1>
+            <h1 className='text-3xl font-bold text-secondary mt-10 mb-5'>Welcome to Creet Blog - where curiosity meets content.</h1>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <h1 className='text-center text-6xl text-primary font-extrabold max-w-2xl mx-auto p-3'>STAY TUNED</h1>
+    </div>
   )
 }
 
